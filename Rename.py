718036fbbd8b54ecec3c76b538d26599e4ad72e3
BlueWaterTurtle <toddkbonner@yahoo.com>
2024-12-09 14:22:32 -0500

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,4 +1,5 @@
 #program for monitoring a folder and renaming any new files that enter the directory
+
 import os
 import time
 import shutil
@@ -8,12 +9,23 @@ from watchdog.events import FileSystemEventHandler
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         print(f"New file created: {event.src_path}")
-        destination_dir = 'C:\\Users\\Public\\Documents\\ProcessedWaves'  # Define the destination directory
-        os.makedirs(destination_dir, exist_ok=True)  # Create the directory if it doesn't exist
-        shutil.copy(event.src_path, destination_dir)  # Copy the file to the destination directory
+        destination_dir = 'C:\\Users\\Public\\Documents\\ProcessedWaves'                  # Define the destination directory
+        os.makedirs(destination_dir, exist_ok=True)                                       # Create the directory if it doesn't exist
+
+                                                                                         
+        file_name, file_extension = os.path.splitext(os.path.basename(event.src_path))    # Extracting file name and extension
+
+         # Define the new name and path
+        new_name = f"{file_name}_renamed{file_extension}"
+        destination_path = os.path.join(destination_dir, new_name)
+
+
+        
+        shutil.copy(event.src_path, destination_dir)                                      # Copy the file to the destination directory
+        print(f"File copied and renamed to: {destination_path}")
 
 def main():
-    path = 'C:\\Users\Public\\Documents\\Waves' #I have a testing directory setup in my laptop
+    path = 'C:\\Users\Public\\Documents\\Waves'                                           #I have a testing directory setup in my laptop
     os.makedirs(path, exist_ok=True)  # Create the directory if it doesn't exist
     event_handler = MyHandler()
     observer = Observer()
