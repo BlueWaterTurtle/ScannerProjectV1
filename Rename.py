6d21fe99e3ccddc4a9436f2c53301b82dea7e107
BlueWaterTurtle <toddkbonner@yahoo.com>
2024-12-09 14:12:26 -0500

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,12 +1,16 @@
 #program for monitoring a folder and renaming any new files that enter the directory
 import os
 import time
+import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         print(f"New file created: {event.src_path}")
+        destination_dir = 'C:\\Users\\Public\\Documents\\ProcessedWaves'  # Define the destination directory
+        os.makedirs(destination_dir, exist_ok=True)  # Create the directory if it doesn't exist
+        shutil.copy(event.src_path, destination_dir)  # Copy the file to the destination directory
 
 def main():
     path = 'C:\\Users\Public\\Documents\\Waves' #I have a testing directory setup in my laptop
