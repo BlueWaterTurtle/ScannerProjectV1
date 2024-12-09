940f7e2a54d02e2c6ae3251cdd8c38f8b0b67fac
BlueWaterTurtle <toddkbonner@yahoo.com>
2024-12-09 13:47:32 -0500

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,7 +1,7 @@
 #program for monitoring a folder and renaming any new files that enter the directory
 import os
 import time
-from watchdog.observer import Observer
+from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
@@ -12,7 +12,7 @@ def main():
     path = 'C:\\Users\Public\\Documents\\Waves' #I have a testing directory setup in my laptop
     os.makedirs(path, exist_ok=True)  # Create the directory if it doesn't exist
     event_handler = MyHandler()
-    observer = Observers()
+    observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
 
